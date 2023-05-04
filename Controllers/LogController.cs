1870f25b35b28f83d4bc2eb2d08a0e1e0681ba2c
Fatih SAÄLAM <fatih.sglmm@gmail.com>
2023-05-04 15:25:07 +0300

    Add project files.

---
+++ Controllers/LogController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.Serilog.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogInformation()
+        {
+            await Task.Delay(500);
+            _logger.LogInformation("Used Logger for information");
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogError()
+        {
+            try
+            {
+                await Task.Delay(500);
+                throw new Exception("There was an error");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Used Logger for error", $"{ex.Message} , {ex.StackTrace}");
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ Extensions/SerilogConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace Learn.Serilog.Extensions
+{
+    public static class SerilogConfiguration
+    {
+        public static void LoggingService(this WebApplicationBuilder builder, IServiceCollection services)
+        {
+            builder.Host.UseSerilog((context , services , configuration) => 
+            configuration.ReadFrom.Configuration(context.Configuration));
+            services.AddLogging();
+        }
+    }
+}
